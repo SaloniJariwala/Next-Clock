74355939e4c747debda1f8f8319c1b2be254217c
123Hetal <110082162+123Hetal@users.noreply.github.com>
2023-01-22 18:18:47 +0530

    update

--- components/StopWatch/DisplayStopWatch.jsx
+++ components/StopWatch/DisplayStopWatch.jsx
@@ -5,9 +5,7 @@ const DisplayStopWatch = (props) => {
    
     return (
             <div className={styles.display_time}>
-                <span>{(props.time.minute >= 10) ? props.time.minute : "0" + props.time.minute}:</span>
-                <span>{(props.time.second >= 10) ? props.time.second : "0" + props.time.second}</span>
-                <span className={styles.display_milisecond}>{(props.time.milisecond >= 10) ? props.time.milisecond : "0" + props.time.milisecond}</span>
+            <span>{props.msToTime(props.time)}</span>
             </div>
     )
 }
