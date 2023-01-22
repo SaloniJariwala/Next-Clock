41db5065c6ad3367d7a1c5fb23c0922fa910999c
123Hetal <110082162+123Hetal@users.noreply.github.com>
2023-01-22 18:14:01 +0530

    stopwatch

--- components/StopWatch/index.jsx
+++ components/StopWatch/index.jsx
@@ -1,170 +1,142 @@
-import React, { useEffect, useState } from "react";
-import { CSVLink } from "react-csv";
-import { Divider } from "antd";
-import styles from "../../styles/StopWatch.module.css";
-import BtnStopWatch from "./BtnStopWatch";
+        
+import { useEffect, useState } from "react";
+import styles from "../../styles/StopWatch.module.css"
 import DisplayStopWatch from "./DisplayStopWatch";
-import { BiExport } from "react-icons/bi";
+import BtnStopWatch from "./BtnStopWatch";
+import { Divider } from "antd";
 
 const StopWatch = ({data}) => {
-  const [isInaterval, setIsIntaerVal] = useState();
-  const [status, setStatus] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-  const [lapTime, setlapTime] = useState([]);
-  const [flag, setFlag] = useState(false);
-  const [csvData, setCsvData] = useState([]);
+  console.log(data);
 
-  const [time, setTime] = useState({
-    lastId: 0,
-    milisecond: 0,
-    second: 0,
-    minute: 0,
-    hour: 0,
-  });
-  let updtaeMs = time.milisecond;
-  let updateSecond = time.second;
-  let updateMinute = time.minute;
-  let updateHour = time.hour;
-  let lastId = 1;
+  const [isActive, setIsActive] = useState(false);
+    const [time, setTime] = useState(0);
+    const [interval, setIsInterval] = useState();
+    const [ispaused, setIsPaused] = useState(false);
+    const [islap, setlap] = useState([]);
+    const [totalTime, setTotalTime] = useState([]);
+    // const [sum, setSum] = useState([0]); 
+  
+    const lap = () => {
+      const getTime = JSON.parse(localStorage.getItem("time")) || [];
+      setTime((time) => time + 10);
+      getTime.push(time);
+      getTime.reverse();
+      // getTime.reverse();
+      localStorage.setItem("time", JSON.stringify(getTime));
+      setlap(getTime);
+    };
+    useEffect(() => {
+      setlap(JSON.parse(localStorage.getItem("time")) || []);
+      if (isActive && ispaused === false) {
+        setIsInterval(
+          setInterval(() => {
+            setTime((time) => time + 10);
+          }, 10)
+        );
+      }
+      return clearInterval(interval);
+    }, [isActive, ispaused]);
+  
+    const handleStart = () => {
+      setIsActive(true);
+    };
 
-  // set stopwatch Time
-  const setStopWatchTime = () => {
-    if (updateMinute === 60) {
-      updateHour++;
-      updateMinute = 0;
+    const Resume=()=>{
+      setIsActive(false);
+      setIsPaused(false);
+      handleStart();
     }
-    if (updateSecond === 60) {
-      updateMinute++;
-      updateSecond = 0;
-    }
-    if (updtaeMs === 60) {
-      updateSecond++;
-      updtaeMs = 0;
-    }
-    updtaeMs++;
-    return setTime({
-      milisecond: updtaeMs,
-      second: updateSecond,
-      minute: updateMinute,
-      hour: updateHour,
-    });
+  
+    const handleReset = () => {
+      setIsActive(false);
+      setTime(0);
+    };
+  
+    const stop = () => {
+      setIsPaused(!ispaused);
+      handleStart();
+    };
+  
+    const totalSum = (item) => {
+      debugger
+      let total = 0;
+      debugger
+      totalTime.forEach((time) => {
+        total=time;
+        total = total + time;
+      });
+      debugger
+      let sum = 0;
+      debugger
+      sum = total + item;
+      debugger
+      totalTime.push(sum);
+      debugger
+      return sum;
+    };
+  
+    const convertTime = (time) => {
+      const sum = `${("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:${(
+        "0" +
+        (Math.floor(time / 1000) % 60)
+      ).slice(-2)}:${("0" + (Math.floor(time / 10) % 100)).slice(-2)}`;
+      return sum;
+    };
+  
+    function msToTime(time) {
+      var milliseconds = parseInt((time % 1000) / 100)
+          , seconds = parseInt((time / 1000) % 60)
+          , minutes = parseInt((time / (1000 * 60)) % 60)
+          , hours = parseInt((time / (1000* 60 *60)) % 24);
+  
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      seconds = (seconds < 10) ? "0" + seconds : seconds;
+  
+      return  minutes + ":" + seconds + "." + milliseconds;
   };
-
-  // stopwatsh start
-  const startStopWatch = () => {
-    if (!isRunning) {
-      setIsRunning(true);
-      setStopWatchTime();
-      setIsIntaerVal(setInterval(setStopWatchTime, 10));
-      setStatus(1);
-    }
-  };
-
-  const stop = () => {
-    setIsRunning(false);
-    clearInterval(isInaterval);
-    setStatus(2);
-  };
-
-  // stopwatsh stop
-  const reset = () => {
-    clearInterval(isInaterval);
-    setStatus(0);
-    setTime({ milisecond: 0, second: 0, minute: 0, hour: 0 });
-    localStorage.removeItem("stopWatchLap");
-  };
-
-  // stopwatsh resume
-  const resume = () => {
-    setStatus(2);
-    startStopWatch();
-    const getTime = JSON.parse(localStorage.getItem("stopWatchLap")) || [];
-    setlapTime(getTime);
-  };
-
-  // display timing
-  const lap = () => {
-    setIsRunning(true);
-    const allStopTime = JSON.parse(localStorage.getItem("stopWatchLap")) || [];
-    startStopWatch();
-    allStopTime.push(time);
-    allStopTime.reverse();
-    localStorage.setItem("stopWatchLap", JSON.stringify(allStopTime));
-    setlapTime(
-      localStorage.setItem("stopWatchLap", JSON.stringify(allStopTime))
-    );
-    setFlag(!flag);
-    setCsvData(allStopTime);
-  };
-
-  useEffect(() => {
-    const allStopTime = JSON.parse(localStorage.getItem("stopWatchLap")) || [];
-    setlapTime(allStopTime);
-    setFlag(flag);
-  }, [flag]);
-
   return (
     <div className={styles.stopWatch}>
       <div className={styles.stopwatch_timer}>
-        <DisplayStopWatch time={time} />
-      </div>
-      <BtnStopWatch
-        startStopWatch={startStopWatch}
-        status={status}
+         <DisplayStopWatch time={time} msToTime={msToTime} />
+       </div>
+       <BtnStopWatch
+        isActive={isActive}
+        ispaused={ispaused}
+        handleStart={handleStart}
         stop={stop}
-        reset={reset}
-        resume={resume}
-        isRunning={isRunning}
+        handleReset={handleReset}
         lap={lap}
+        Resume={Resume}
       />
       <Divider />
-      {/* display lap his */}
-      <div className={styles.lap_timing}>
-        <div className={styles.lap_title}>LapHistory</div>
-        <div>
-          {lapTime?.length > 0 && (
-            <>
-              <table className={styles.lap_table}>
-                <thead>
-                  <tr>
-                    <th className={styles.lap_table_th}>Time</th>
-                    <th className={styles.lap_table_th}>Total Time</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {lapTime &&
-                    lapTime.map((time) => (
-                      <tr>
-                        <td className={styles.lap_table_td}>
-                          {time.minute}:{time.second}:{time.milisecond}
-                        </td>
-                        <td className={styles.lap_table_td}></td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </>
-          )}
-        </div>
-      </div>
-      <div>
-        <CSVLink
-          data={csvData}
-          style={{ width: "100%", textDecoration: "none" }}
-        >
-          <button className={styles.csv_button}>
-            <BiExport />
-            Export To CSV
-          </button>
-        </CSVLink>
-
-      <div className={styles.stopwatch_text}>{data}</div>
-      </div>
+      
+      <table>
+      <thead>
+      <tr>
+            <th>Lap</th>
+            <th>Time</th>    
+            <th>Total</th>
+          </tr>
+      </thead>
+      <tbody>
+      <td>
+      {islap?.map((item, index) => (
+        <tr key={index}>
+          <td>
+            {("0" + (Math.floor(item / 60000) % 60)).slice(-2)}:
+            {("0" + (Math.floor(item / 1000) % 60)).slice(-2)}:
+            {("0" + (Math.floor(item / 10) % 100)).slice(-2)}
+          </td>
+              <td>{convertTime(totalSum(item))}</td>
+        </tr>
+      ))}
+        </td>
+      </tbody>
+      </table>
 
     </div>
-  );
-};
-
-
+  )
+}
 
 export default StopWatch;
