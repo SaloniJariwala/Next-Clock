a881e23d5d92333b1c3c4aba51f14cf908502207
123Hetal <110082162+123Hetal@users.noreply.github.com>
2023-01-22 18:18:09 +0530

    update

--- components/StopWatch/BtnStopWatch.jsx
+++ components/StopWatch/BtnStopWatch.jsx
@@ -1,69 +1,50 @@
 import React from "react";
-import styles from "../../styles/StopWatch.module.css";
+import styles from "../../styles/StopWatch.module.css"
 import { BsSkipStart } from "react-icons/bs";
 import { MdOutlineRestartAlt, MdPause, MdTimelapse } from "react-icons/md";
 import { GrResume } from "react-icons/gr";
 
-const BtnStopWatch = (props) => {
+const BtnStopWatch = ({ isActive, ispaused, handleStart, Resume,stop, handleReset, lap }) => {
     return (
         <>
-            <div className={styles.stopwatch_button}>
-                {props.status === 0 && (
+            <div className={styles.display_timer_button}>
+                {!isActive ? (
+                    <button
+                        className={styles.btn_start}
+                        onClick={handleStart}
+                    >
+                        <BsSkipStart />
+                        Start
+                    </button>
+                ) : (
                     <>
-                        <div>
-                            <button
-                                className={styles.btn_start}
-                                onClick={props.startStopWatch}
-                            >
-                                <BsSkipStart />
-                                Start
-                            </button>
-                        </div>
-                        <div>
-                            <button className={styles.btn_reset} 
-                                    onClick={props.reset}>
-                                <MdOutlineRestartAlt />
-                                Reset
-                            </button>
-                        </div>
-                    </>
-                )}
+                        {ispaused ? (
+                            <>
+                                <button className={styles.btn_start} onClick={Resume}>
+                                    <BsSkipStart />
+                                    Resume
+                                </button>
+                            </>
+                        ) : (
+                            <>
+                            <button className={styles.btn_reset}
+                            onClick={lap}>
+                            <MdTimelapse />
+                            Lap
+                        </button>
 
-                {props.status === 1 && (
-                    <>
-                        <div>
-                            <button className={styles.btn_start} 
-                                    onClick={props.stop}>
-                                <MdPause />
-                                stop
-                            </button>
-                        </div>
-                        <div>
-                            <button className={styles.btn_reset} 
-                                    onClick={props.lap}>
-                                <MdTimelapse />
-                                Lap
-                            </button>
-                        </div>
-                    </>
-                )}
-
-                {props.status === 2 && (
-                    <>
-                        <div>
-                            <button className={styles.btn_start} 
-                                    onClick={props.resume}>
-                                <GrResume />
-                                Resume
-                            </button>
-                        </div>
-                        <div>
-                            <button className={styles.btn_reset} 
-                                    onClick={props.reset}>
-                                <MdOutlineRestartAlt />
-                                Reset
-                            </button>
-                        </div>
+                        <button className={styles.btn_start}
+                        onClick={stop}>
+                        <MdPause />
+                        stop
+                    </button>
+                            </>
+                        )}
+                        <button className={styles.btn_reset}
+                            onClick={handleReset}>
+                            <MdOutlineRestartAlt />
+                            Reset
+                        </button>
                     </>
                 )}
             </div>
